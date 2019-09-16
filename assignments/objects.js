e3f2c34553abf86bc926b16b5a40a7e51fcff36c
Devanee Reid <devaneereid@gmail.com>
2019-09-16 14:50:53 -0600

    answered all questions but one on arrays page

--- assignments/objects.js
+++ assignments/objects.js
@@ -72,22 +72,22 @@ console.log(intern5.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-const kennan = {
-  speak: function() {
+const intern2Speak = {
+  speak: () => {
     return "Hello, my name is Kennan!";
   }
 };
 
-console.log(kennan.speak());
+console.log(intern2Speak.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-const antonietta = {
+const intern5Math = {
   multiplyNums: function(num1, num2) {
     return num1 * num2;
   }
 };
-console.log(antonietta.multiplyNums(3,4));
+console.log(intern5Math.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
